70d6a86bba31e2825299f46a4ef9c16afa023390
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-08-13 13:56:10 -0400

    Update js.js

--- needed/js.js
+++ needed/js.js
@@ -1,3 +1,7 @@
 function start() {
-            document.getElementById("text").innerHTML = "How did you find me?"
+            document.getElementById("text").innerHTML = "That was obvious"
+            var a = prompt("???: ")
+            if (a === "yes") {
+                        document.getElementsByname("Python").innerHTML = "-... ..- - / .. ... / - .... .. ... ..--.."
+            }
         }
