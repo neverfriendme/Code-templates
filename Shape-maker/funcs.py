d069357e2bcbeae8616a998c4643a98f8d89219a
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-08-16 16:11:10 -0400

    Create funcs.py

---
+++ Shape-maker/funcs.py
@@ -0,0 +1,47 @@
+# by neverfriendme
+from turtle import *
+
+
+def Form_cric():
+    if 1 == 1:
+        while True:
+            forward(3.14)
+            right(3.14)
+def Form_octa():
+    if 1 == 1:
+        while True:
+            forward(90)
+            left(45)
+            right(90)
+def Form_nona():
+    if 1 == 1:
+        while True:
+            right(90)
+            left(50)
+            forward(90)
+def listfunc_form():
+    print(".nona\n.circ\n.octa\n.hexa\n.dode\nMore updates coming soon!")
+
+def help():
+    print("listfunc.[Function] not listfunc.Function\nhelp or ?h to get Help")
+
+def Form_hexa():
+    area = 3/3 * 10 * 20
+    if 1 == 1:
+        while True:
+            forward(area)
+            left(20)
+            right(area * 1.6)
+
+def Form_dode():
+    area = 1/4 * 10 * 15
+    if 1 == 1:
+        while True:
+            forward(area)
+            left(30)
+            right(area * 1.6)
+
+
+
+
+            
