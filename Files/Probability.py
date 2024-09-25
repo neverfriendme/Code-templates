185f6a9368df0c87a711bbdce12abc8ce2fae290
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-09-25 19:08:31 -0400

    Update Probability.py

--- Files/Probability.py
+++ Files/Probability.py
@@ -3,10 +3,10 @@ import time
 # by neverfriendme
 def probability():
     try:
-        Range = int(input("Range: 1 now enter your number:\n"))
+        Range = int(input("Range = 0 , enter a number:\n"))
         if 1 == 1:
             while True:
-                generate = randrange(1,Range)
+                generate = randrange(0,Range)
                 time.sleep(1)
                 print(generate)
     except ValueError:
