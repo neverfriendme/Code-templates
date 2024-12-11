9ef763a8993bc7a1c2d2067013451e0ec7f2d38b
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-12-11 16:45:25 -0500

    Update Circle_Area_Calculator.py

--- Files/Circle_Area_Calculator.py
+++ Files/Circle_Area_Calculator.py
@@ -2,7 +2,7 @@
 import math
 def radius_calc_circ():
   try:
-    r = int(input("Radius:\n")
+    r = int(input("Radius:\n"))
     r *= r
     a = r*math.pi
     print(f"The area of the circle is {a}")
