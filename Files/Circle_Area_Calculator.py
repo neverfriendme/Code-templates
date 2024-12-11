46fb1c854a2d154f6ac0f511710c09f166328417
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-12-11 16:40:16 -0500

    Create Circle_Area_Calculator.py

---
+++ Files/Circle_Area_Calculator.py
@@ -0,0 +1,10 @@
+#by:neverfriendme
+import math
+def radius_calc_circ():
+  try:
+    r = int(input("Radius:\n")
+    r *= r
+    a = r*math.pi
+    print(f"The area of the circle is {a}")
+  except ValueError:
+    print("Incorrect Value")
