e878828dd7b0f08f85136b36bc840fdee1db7e84
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-12-25 15:32:13 -0500

    Update Creditcard_masker.py

--- Files/Creditcard_masker.py
+++ Files/Creditcard_masker.py
@@ -1,14 +1,4 @@
 # by:neverfriendme
 def Credit_card_masker(card):
-    if card[15] not in card:
-        quit()
-    elif card[15] in card:
-        pass
-
     print("*" * 12 + card[12:16])
-
-
-
-
-
-Credit_card_masker("1234567890123456")
+Credit_card_masker("1234567890123457")
