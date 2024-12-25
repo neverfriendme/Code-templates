878e04bff7b216e8f96bee455c7077cea3c89b83
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-12-25 13:37:28 -0500

    Update Creditcard_masker.py

--- Files/Creditcard_masker.py
+++ Files/Creditcard_masker.py
@@ -1,8 +1,14 @@
 # by:neverfriendme
-str = "1234567890123456"
-# string to mask
-i = "*"
-# "*" for masking
-print(i * 12+str[12]+str[13]+str[14]+str[15])
-# i * 12 because those are the ones we are not going to show.
-# then letters 12,13,14,15 because those are the 4 we want to show.
+def Credit_card_masker(card):
+    if card[15] not in card:
+        quit()
+    elif card[15] in card:
+        pass
+
+    print("*" * 12 + card[12:16])
+
+
+
+
+
+Credit_card_masker("1234567890123456")
