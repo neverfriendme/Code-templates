3afe2f6f8f76a6d497aadb8b8fd05011fd7ce475
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2024-12-26 12:00:54 -0500

    Update Creditcard_masker.py

--- Files/Creditcard_masker.py
+++ Files/Creditcard_masker.py
@@ -1,6 +1,4 @@
 # by:neverfriendme
 def Credit_card_masker(card):
-    try:
-        print("*" * 12 + card[12:16])
-    except IndexError:
-        print("Invalid")
+    print("*" * 12 + card[12:16])
+
