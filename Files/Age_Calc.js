43da832082151f0fa14e619300fee29bc7134915
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2025-02-23 09:39:22 -0500

    Update Age_Calc.js

--- Files/Age_Calc.js
+++ Files/Age_Calc.js
@@ -2,4 +2,4 @@
 Month = Number(prompt("Month of Birth:\n"))
 Year = Number(prompt("Year of Birth:\n"))
 Now = Number(prompt("Present:\n"))
-alert("You were born " + Now - Year + " and " + 12 - Month +" ago")
+document.write("You were born " + Now - Year + " and " + 12 - Month +" ago")
