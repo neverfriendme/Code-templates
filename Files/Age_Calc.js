04d2888fe75722ae36f5293dfdf9c0e51862064f
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2025-02-23 09:36:24 -0500

    Update Age_Calc.js

--- Files/Age_Calc.js
+++ Files/Age_Calc.js
@@ -1,6 +1,5 @@
 // made from: neverfriendme
-Months = [0,12,11,10,9,8,7,6,5,4,3,2,1]
 Month = Number(prompt("Month of Birth:\n"))
 Year = Number(prompt("Year of Birth:\n"))
 Now = Number(prompt("Present:\n"))
-alert("You were born " + Now - Year + " and " + Months - Month +" ago")
+alert("You were born " + Now - Year + " and " + 12 - Month +" ago")
