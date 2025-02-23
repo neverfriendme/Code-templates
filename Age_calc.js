81c6a9e45d7e23c76cb57490e2f0d6fba44e22fc
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2025-02-22 22:12:38 -0500

    Delete Age_calc.js

--- Age_calc.js
+++
@@ -1,5 +0,0 @@
-Months = [0,12,11,10,9,8,7,6,5,4,3,2,1]
-Month = Number(prompt("Month of Birth:\n"))
-Year = Number(prompt("Year of Birth:\n"))
-Now = Number(prompt("Present:\n"))
-alert("You are " + Now - Year, Months - Month)
