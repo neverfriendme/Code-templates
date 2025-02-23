c74a6d84703539ac8c88bd02d5db6392b8b60fd8
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2025-02-23 09:50:55 -0500

    Update Age_Calc.js

--- Files/Age_Calc.js
+++ Files/Age_Calc.js
@@ -2,9 +2,9 @@
 let Month = Number(prompt("Month of Birth:\n"));
 let Year = Number(prompt("Year of Birth:\n"));
 let Now = Number(prompt("Present:\n"));
-let present = Number(prompt("Year now:"))
-let monthage = 12 - Month
-let yearage = Now - Year
-document.write("You were born " + yearage + "and " + monthage + " ago")
+let present = Number(prompt("Year now:"));
+let monthage = 12 - Month;
+let yearage = Now - Year;
+document.write("You were born " + yearage + "and " + monthage + " ago");
 
 
