fd68fb5dd68e28f75c69111843dfe447a5af388e
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2025-02-23 09:35:34 -0500

    Update Age_Calc.js

--- Files/Age_Calc.js
+++ Files/Age_Calc.js
@@ -3,4 +3,4 @@ Months = [0,12,11,10,9,8,7,6,5,4,3,2,1]
 Month = Number(prompt("Month of Birth:\n"))
 Year = Number(prompt("Year of Birth:\n"))
 Now = Number(prompt("Present:\n"))
-alert("You are " + Now - Year, Months - Month)
+alert("You were born " + Now - Year + " and " + Months - Month +" ago")
