b50e314a62617e3891714efa646f784f8d941c80
Maybe? <154563439+neverfriendme@users.noreply.github.com>
2025-07-20 12:48:31 -0400

    Update Password_gen.py

--- Files/Password_gen.py
+++ Files/Password_gen.py
@@ -1,6 +1,6 @@
 List1 = "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"
-List2 = "apple", "banana", "dolphin", "beach", "stream","Tube","Norse","Nurse","engineer","fear","happy","grow","sun","moon","cool"
-List3 = "!", "@", ",", ".", "#", "/", "*","-", "—"
+List2 = "blooded", "banana", "dolphin", "breath", "stream","facade","Norses","Nurses","engineer","feared","happiness","growth","solar_system","lunatic","cooled","chilled","filled","Batman","Superman","Shovel"
+List3 = "!", "@", ",", ".", "#", "/", "*","-", "—", "~", "|","?", ":", ";","%", "^", "$", "&","`","+","=","_"
 
 generatelist1 = random.choice(List1)
 generatelist2 = random.choice(List2)
